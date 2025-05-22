33d986bb2d8a95e4bbe9e64c78c14ec0f3f6c188
lemongza <92498054+lemongza@users.noreply.github.com>
2025-05-22 15:38:37 +0900

    handler 함수

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -6,9 +6,17 @@ export default class MyComponent extends Component {
   //상태변수를 포함하는 state 객체 선언
   state = {
     value: 0,
+    message: "",
+    username: "",
   };
   //Event Handler
   handleDecrement = () => this.setState({ value: this.state.value - 1 });
+  
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value, //변수이름 동적으로 처리 [e.target.name] name=> message, username
+    });
+  };
 
   //Event Handler 2
   // -> bind(this)를 생성자에서 해주어야 this 사용 가능
@@ -19,8 +27,8 @@ export default class MyComponent extends Component {
   render() {
     //destructuring assignment
     const { name, age } = this.props;
-    const { value } = this.state;
-    const { handleDecrement } = this;
+    const { value, message, username } = this.state;
+    const { handleDecrement, handleChange } = this;
 
     return (
       <div>
@@ -33,6 +41,12 @@ export default class MyComponent extends Component {
           증가
         </button>
         <button onClick={handleDecrement}>감소</button>
+        <br />
+        <p>State message의 값 = {message}</p>
+        <input name="message" value={message} onChange={handleChange}></input>
+        <br />
+        <p>State username의 값 = {username}</p>
+        <input name="username" value={username} onChange={handleChange}></input>
       </div>
     );
   }
